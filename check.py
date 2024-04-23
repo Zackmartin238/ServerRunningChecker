bc64be180188124708c3a3e374156136987e77ea
Zack Martin <zackmartin238@gmail.com>
2024-04-23 13:58:20 -0500

    commit

--- check.py
+++ check.py
@@ -1,19 +1,23 @@
 import subprocess 
 import datetime
-  
-pytonProcess = subprocess.check_output("ps aux | grep spigot",shell=True).decode() 
-pytonProcess = pytonProcess.split('\n') 
+
+pythonProcess = subprocess.check_output("ps aux | grep spigot", shell=True).decode() 
+pythonProcess = pythonProcess.split('\n') 
 currently_Running = False  
-for process in pytonProcess: 
+for process in pythonProcess: 
     print(process)
     if 'spigot' in process:
         currently_Running = True
         good_process = process
-if currently_Running == True:
-        print("["+datetime.datetime()+f"] Lookin good. It's running: {good_process}")
+
+if currently_Running:
+    print(f"[{datetime.datetime.now()}] Lookin good. It's running: {good_process}")
 else:
     print("Well, it's not running... maybe try restarting?")    
-    restartshell = subprocess.Popen("su root", Shell=True)
-    restartshell.stdin.write("kali \n")
-    tryRunning = subprocess.check_output("readlink -f start.sh", shell=True).decode()
-    restartshell.stdin.write(tryRunning)
+    try:
+        restartshell = subprocess.Popen(["su", "root"], stdin=subprocess.PIPE, shell=True)
+        restartshell.communicate(input="kali\n".encode())
+        tryRunning = subprocess.check_output("readlink -f start.sh", shell=True).decode().strip()
+        restartshell.communicate(input=tryRunning.encode())
+    except Exception as e:
+        print(f"An error occurred: {e}")
