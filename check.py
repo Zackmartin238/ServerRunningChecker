b2c2b331655b48d2a271e7b995e29c7cc94497c0
Zack Martin <zackmartin238@gmail.com>
2024-04-23 14:09:20 -0500

    comm

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 import subprocess 
 import datetime
 
+# Check if the 'spigot' process is running
 pythonProcess = subprocess.check_output("ps aux | grep spigot", shell=True).decode() 
 pythonProcess = pythonProcess.split('\n') 
 currently_Running = False  
@@ -10,17 +11,17 @@ for process in pythonProcess:
         currently_Running = True
         good_process = process
 
+# If 'spigot' process is running
 if currently_Running:
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("[%Y-%m-%d %H:%M:%S]")
     print(f"[{formatted_time}] Lookin good. It's running: {good_process}")
 else:
     print("Well, it's not running... maybe try restarting?")    
+
+    # Restart the 'spigot' process
     try:
-        restartshell = subprocess.Popen(["sudo", "su", "root"], stdin=subprocess.PIPE)
-        restartshell.communicate(input="kali\n".encode())
-        restartshell.communicate(input="cd\n".encode())
-        tryRunning = subprocess.check_output("readlink -f start.sh", shell=True).decode().strip()
-        subprocess.Popen(tryRunning, shell=True)
+        restart_command = "screen -r main"
+        subprocess.Popen(restart_command, shell=True)
     except Exception as e:
         print(f"An error occurred: {e}")
