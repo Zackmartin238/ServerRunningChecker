4ae45f4afc30ae2843c11e95a1e61ce1aaee4bb5
Zack Martin <zackmartin238@gmail.com>
2024-04-23 14:34:29 -0500

    commit

--- check.py
+++ check.py
@@ -1,28 +1,33 @@
-import subprocess 
+import subprocess
 import datetime
+import time
 
-# Check if the 'spigot' process is running
-pythonProcess = subprocess.check_output("ps aux | grep spigot", shell=True).decode() 
-pythonProcess = pythonProcess.split('\n') 
-currently_Running = False  
-for process in pythonProcess: 
-    print(process)
-    if 'spigot' in process and "grep" not in process:
-        currently_Running = True
-        good_process = process
+while True:
+    # Check if the 'spigot' process is running
+    pythonProcess = subprocess.check_output("ps aux | grep spigot", shell=True).decode()
+    pythonProcess = pythonProcess.split('\n')
+    currently_Running = False
+    for process in pythonProcess:
+        if 'spigot' in process and "grep" not in process:
+            currently_Running = True
+            good_process = process
 
-# If 'spigot' process is running
-if currently_Running:
-    current_time = datetime.datetime.now()
-    formatted_time = current_time.strftime("[%Y-%m-%d %H:%M:%S]")
-    print(f"[{formatted_time}] Lookin good. It's running: {good_process}")
-else:
-    print("Well, it's not running... maybe try restarting?")    
+    # If 'spigot' process is running
+    if currently_Running:
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.strftime("[%Y-%m-%d %H:%M:%S]")
+        print(f"{formatted_time} Lookin good. It's running: {good_process}")
+    else:
+        print("Well, it's not running... maybe try restarting?")
 
-    # Restart the 'spigot' process
-    try:
-        restart_command = "screen -r main"
-        sub = subprocess.Popen(restart_command, shell=True)
-        sub.stdin.write("cd /home/zackmartin238/Spigot && ./start.sh").encode()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        # Restart the 'spigot' process
+        try:
+            restart_command = "screen -r main && cd /home/zackmartin238/Spigot && ./start.sh"
+            subprocess.run(restart_command, shell=True, check=True)
+            print("Restarted successfully!")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e}")
+
+    # Wait for some time before checking again (e.g., 1 minute)
+    time.sleep(60)
+
